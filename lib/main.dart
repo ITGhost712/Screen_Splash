7d2f88ec890825aac2326a1b7e8e1cf410b771ac
unknown <sameenabbasi46@gmail.com>
2023-03-31 00:05:21 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aaziq Ali Shah',
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MainScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'My App',
+          style: TextStyle(
+            fontSize: 50.0,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final List<String> _records = [
+    'Record 1',
+    'Record 2',
+    'Record 3',
+    'Record 4',
+    'Record 5',
+  ];
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Records'),
+      ),
+      body: ListView.builder(
+        itemCount: _records.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(_records[index]),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddRecordDialog();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _showAddRecordDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Add Record"),
+          content: TextField(
+            controller: _textEditingController,
+            decoration: InputDecoration(hintText: "Enter a record"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Add"),
+              onPressed: () {
+                setState(() {
+                  _records.add(_textEditingController.text);
+                });
+                _textEditingController.clear();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
